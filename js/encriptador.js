8e97599a666d91ad3e3784f7983dc1236a6278ed
ayelenleclerc <ayelenleclerc@gmail.com>
2022-05-13 09:52:35 -0300

    modificado encriptador para responsive

--- js/encriptador.js
+++ js/encriptador.js
@@ -1,48 +1,49 @@
-const inputUsuario = document.getElementById("inputUsuario");
-const mostrarMensaje = document.getElementById("mostrarMensaje");
-const copiarBtn = document.getElementById("copiarBtn");
-const mensajeVacio = document.getElementById("mensajeVacio");
-const ocultarMensajeVacio = document.getElementById("ocultarMensajeVacio");
-const ocultarBtnCopiar = document.getElementById("ocultarBtnCopiar");
+const inputUsuario = document.getElementById("inputUsuarioEncrip");
+const mostrarMensaje = document.getElementById("mostrarMensajeEncrip");
+const copiarBtn = document.getElementById("copiarBtnEncrip");
+const mensajeVacio = document.getElementById("mensajeVacioEncrip");
+const ocultarMensajeVacio = document.getElementById(
+  "ocultarMensajeVacioEncrip"
+);
+const ocultarBtnCopiar = document.getElementById("ocultarBtnCopiarEncrip");
 const encriptar = document.getElementById("encriptar");
 const desencriptar = document.getElementById("desencriptar");
-const copiarTexto = document.getElementById("copiarBtn");
-const pegarTexto = document.getElementById("pegarBtn");
+const copiarTexto = document.getElementById("copiarBtnEncrip");
 
 encriptar.addEventListener("click", function () {
-  mensajeVacio.classList.remove("mostrarMensajeVacio");
-  mensajeVacio.classList.add("ocultarMensajeVacio");
-  copiarBtn.classList.remove("ocultarBtnCopiar");
+  mensajeVacio.classList.remove("mostrarMensajeVacioEncrip");
+  mensajeVacio.classList.add("ocultarMensajeVacioEncrip");
+  copiarBtn.classList.remove("ocultarBtnCopiarEncrip");
 
-  let inputUsuario = document.getElementById("inputUsuario").value;
+  let inputUsuario = document.getElementById("inputUsuarioEncrip").value;
   let encriptar = inputUsuario
     .replace(/e/g, "enter")
     .replace(/i/g, "imes")
     .replace(/o/g, "ober")
     .replace(/u/g, "ufat")
     .replace(/a/g, "ai");
-  document.getElementById("mostrarMensaje").innerHTML = encriptar;
-  inputUsuario = document.getElementById("inputUsuario").value = "";
+  document.getElementById("mostrarMensajeEncrip").innerHTML = encriptar;
+  inputUsuario = document.getElementById("inputUsuarioEncrip").value = "";
 });
 
 desencriptar.addEventListener("click", function () {
-  mensajeVacio.classList.remove("mostrarMensajeVacio");
-  mensajeVacio.classList.add("ocultarMensajeVacio");
-  copiarBtn.classList.remove("ocultarBtnCopiar");
+  mensajeVacio.classList.remove("mostrarMensajeVacioEncrip");
+  mensajeVacio.classList.add("ocultarMensajeVacioEncrip");
+  copiarBtn.classList.remove("ocultarBtnCopiarEncrip");
 
-  let inputUsuario = document.getElementById("inputUsuario").value;
+  let inputUsuario = document.getElementById("inputUsuarioEncrip").value;
   let desencriptar = inputUsuario
     .replace(/enter/g, "e")
     .replace(/imes/g, "i")
     .replace(/ober/g, "o")
     .replace(/ai/g, "a")
     .replace(/ufat/g, "u");
-  document.getElementById("mostrarMensaje").innerHTML = desencriptar;
-  inputUsuario = document.getElementById("inputUsuario").value = "";
+  document.getElementById("mostrarMensajeEncrip").innerHTML = desencriptar;
+  inputUsuario = document.getElementById("inputUsuarioEncrip").value = "";
 });
 
 copiarTexto.addEventListener("click", function () {
-  let textoCopiado = document.getElementById("mostrarMensaje");
+  let textoCopiado = document.getElementById("mostrarMensajeEncrip");
   textoCopiado.select();
   document.execCommand("copy");
 });
