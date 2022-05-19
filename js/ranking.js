ea556bb9ab5bed40c7553fea927a5e44d9f9c658
ayelenleclerc <ayelenleclerc@gmail.com>
2022-05-19 18:22:13 -0300

    capturando el localstorage en Ranking

--- js/ranking.js
+++ js/ranking.js
@@ -6,34 +6,50 @@ class Juego {
     this.puntaje = puntaje;
   }
 }
+const juego1 = new Juego(
+  "Adivinador",
+  "Juego de adivinar un numero",
+  "../img/adivinador/giphy-Adivinador.gif",
+  0
+);
+const juego2 = new Juego(
+  "Memotest",
+  "Juego de encontrar el igual",
+  "../img/memotest/giphy-memotest.gif",
+  0
+);
+const juego3 = new Juego(
+  "Encriptador",
+  "Juego de encriptar un mensaje",
+  "../img/encriptador/giphy-encriptador.gif",
+  0
+);
 
-const juegos = [
-  {
-    nombre: "Adivinador",
-    descripcion: "Juego de adivinar un numero",
-    img: "../img/adivinador/giphy-Adivinador.gif",
-    puntaje: 0,
-  },
-  {
-    nombre: "Memotest",
-    descripcion: "Juego de encontrar el igual",
-    img: "../img/memotest/giphy-memotest.gif",
-    puntaje: 0,
-  },
-  {
-    nombre: "Encriptador",
-    descripcion: "Juego de encriptar un mensaje",
-    img: "../img/encriptador/giphy-encriptador.gif",
-    puntaje: 0,
-  },
-];
+const juegos = [juego1, juego2, juego3];
 
-let puntajeUsuario = [];
+let puntajeAdivinador = [];
+let puntajeMemotest = [];
+let puntajeEncriptador = [];
 
-if (localStorage.getItem("puntajeUsuario")) {
-  puntajeUsuario = JSON.parse(localStorage.getItem("puntajeUsuario"));
+if (localStorage.getItem("puntajeAdivinador")) {
+  puntajeAdivinador = JSON.parse(localStorage.getItem("puntajeAdivinador"));
 } else {
-  localStorage.setItem("puntajeUsuario", JSON.stringify(puntajeUsuario));
+  localStorage.setItem("puntajeAdivinador", JSON.stringify(puntajeAdivinador));
+}
+
+if (localStorage.getItem("puntajeMemotest")) {
+  puntajeMemotest = JSON.parse(localStorage.getItem("puntajeMemotest"));
+} else {
+  localStorage.setItem("puntajeMemotest", JSON.stringify(puntajeMemotest));
+}
+
+if (localStorage.getItem("puntajeEncriptador")) {
+  puntajeEncriptador = JSON.parse(localStorage.getItem("puntajeEncriptador"));
+} else {
+  localStorage.setItem(
+    "puntajeEncriptador",
+    JSON.stringify(puntajeEncriptador)
+  );
 }
 
 let rankingCard = document.getElementById("app");
@@ -45,7 +61,52 @@ juegos.forEach((juego, indice) => {
       <img src="${juego.img} " class="card-img-top" alt="gif ${juego.nombre} ">
   <div class="card-body">
     <p class="card-text">${juego.descripcion} </p>
+    <p class="card-text" id="puntaje${indice}">Puntaje: ${juego.puntaje}</p>
     <button type="button" class="btn btn-dark" id="boton${indice}"> Puntuar </button>
   </div>
 </div> `;
 });
+
+juegos.forEach((juego, indice) => {
+  let botonPuntaje = document.getElementById(`boton${indice}`);
+  botonPuntaje.addEventListener("click", () => {
+    let puntaje = juego.puntaje + 1;
+    juego.puntaje = puntaje;
+    switch (indice) {
+      case 0:
+        localStorage.setItem("puntajeAdivinador", JSON.stringify(puntaje));
+        break;
+      case 1:
+        localStorage.setItem("puntajeMemotest", JSON.stringify(puntaje));
+        break;
+      case 2:
+        localStorage.setItem("puntajeEncriptador", JSON.stringify(puntaje));
+        break;
+    }
+  });
+});
+
+function capturarPuntajesLocalStorage() {
+  juegos.forEach((juego, indice) => {
+    let puntajeLocal = document.getElementById(`puntaje${indice}`);
+    switch (indice) {
+      case 0:
+        puntajeLocal.innerHTML = `Puntaje: ${parseInt(
+          JSON.parse(localStorage.getItem("puntajeAdivinador"))
+        )}`;
+        break;
+      case 1:
+        puntajeLocal.innerHTML = `Puntaje: ${parseInt(
+          JSON.parse(localStorage.getItem("puntajeMemotest"))
+        )}`;
+        break;
+      case 2:
+        puntajeLocal.innerHTML = `Puntaje: ${parseInt(
+          JSON.parse(localStorage.getItem("puntajeEncriptador"))
+        )}`;
+        break;
+    }
+  });
+}
+
+capturarPuntajesLocalStorage();
