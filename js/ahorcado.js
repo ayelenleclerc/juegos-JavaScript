f75c76acdca84d69f31f625ad6a5fa01811b933f
ayelenleclerc <ayelenleclerc@gmail.com>
2022-06-03 09:50:36 -0300

    corrigiendo ruta de json otra vez

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -52,7 +52,7 @@ let imagen = document.getElementById("imagen");
 //FUNCIONES
 
 //Traigo la lista de palabras del JSON
-fetch("./palabrasAhorcado.json")
+fetch("../js/palabrasAhorcado.json")
   .then((res) => res.json())
   .then((palabrasAhorcado) => {
     palabrasAhorcado.forEach((palabrasAhorcado) => {
